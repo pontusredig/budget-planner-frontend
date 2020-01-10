853ca82c16e1464fec9316f7728de93da2bf08ed
Alexander Teklemariam <32360968+Mathriok@users.noreply.github.com>
2020-01-10 11:27:12 +0100

    echarts

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -2,5 +2,28 @@
   <div class="expenses">
     <h1>Information about expenses</h1>
     <v-date-picker></v-date-picker>
+    <chart :options="chartOptionsBar"></chart>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'chart',
+  data: () => ({
+    chartOptionsBar: {
+      xAxis: {
+        data: ['Q1', 'Q2', 'Q3', 'Q4']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          type: 'bar',
+          data: [63, 75, 24, 92]
+        }
+      ]
+    }
+  })
+}
+</script>
